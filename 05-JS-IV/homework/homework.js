438c858dca0977f882c7181980f218f31a95c2f8
Geraldine2000 <geraldine.romero.ochoa@gmail.com>
2022-12-28 09:58:33 -0500

    proceso

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,15 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  const nuevoObjeto={
+    nombre: nombre,
+    edad:edad,
+    meow: function () {
+      return "Meow!"
+    }
+    
+  }
+  return nuevoObjeto;
 }
 
 function agregarPropiedad (objeto, property) {
@@ -13,6 +22,11 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  const objeto = {
+    objeto:`null`,
+    property:property
+  }
+  return objeto;
 }
 
 function invocarMetodo (objeto, metodo) {
